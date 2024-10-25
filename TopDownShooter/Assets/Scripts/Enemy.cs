91f634d4fe2255be997abe7becc73df41c08c0d3
Ardirdn21 <aldi@trimatrastudio.com>
2024-10-25 19:20:28 +0700

    Player Health, Enemy Attack, Attack FX

--- TopDownShooter/Assets/Scripts/Enemy.cs
+++ TopDownShooter/Assets/Scripts/Enemy.cs
@@ -6,21 +6,40 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     public NavMeshAgent Agent;
-    public Transform Player;
+    public Player Player;
     public GameObject DeathFX;
 
+    public float AttackRate = 2f;
+    public float AttackDistance = 3f;
+    public float AttackDamage = 0.1f;
+    private float AttackTime = 0f;
+
     void Update(){
-        Agent.SetDestination(Player.position);
+        AttackTime += Time.deltaTime;
+        if(AttackTime >= AttackRate)
+        {
+            AttackTime = 0f;
+            Attack();
+        }
+
+        Agent.SetDestination(Player.transform.position);
+    }
+
+    private void Attack()
+    {
+        if(Vector3.Distance(Player.transform.position, transform.position) <= AttackDistance)
+        {
+            Player.Damage(AttackDamage);
+        }
     }
 
     private void OnCollisionEnter(Collision Col){
-        if(Col.transform.tag == "Bullet"){
-
+        if(Col.transform.tag == "Bullet")
+        {
             if(DeathFX != null)
             {
                 Instantiate(DeathFX, transform.position, transform.rotation);
             }
-
             Destroy(Col.gameObject);
             Destroy(gameObject);
         }
--- TopDownShooter/Assets/Scripts/Player.cs
+++ TopDownShooter/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -9,10 +10,24 @@ public class Player : MonoBehaviour
 
     public Transform GunHole;
     public GameObject Bullet;
+    public GameObject DamageFX;
+    public GameObject DeathFX;
+
+    public Image HealthBar;
+
+    public float Health = 0f;
 
     public float Speed = 1f;
     public float ShootForce = 20f;
+
+    public bool Dead = false;
+
     Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+    public void Start()
+    {
+        HealthBar.fillAmount = Health;
+    }
 
     void Update(){
         transform.position += Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * Speed;
@@ -28,6 +43,29 @@ public class Player : MonoBehaviour
             var bullet = Instantiate(Bullet, GunHole.position, GunHole.rotation);
             bullet.GetComponent<Rigidbody>().velocity = GunHole.forward * 20f;
             Destroy(bullet, 10);
+        }
+    }
+
+    public void Damage(float damage)
+    {
+        if (Dead) return;
+
+        if(DamageFX != null)
+        {
+            Instantiate(DamageFX, transform.position, transform.rotation);
+        }
+        Health -= damage;
+        Health = Mathf.Max(0, Health);
+        HealthBar.fillAmount = Health;
+
+        if(Health <= 0 && !Dead)
+        {
+            if(DeathFX != null)
+            {
+                Instantiate(DeathFX, transform.position, transform.rotation);
+            }
+            gameObject.SetActive(false);
+            Dead = true;
         }
     }
 }
